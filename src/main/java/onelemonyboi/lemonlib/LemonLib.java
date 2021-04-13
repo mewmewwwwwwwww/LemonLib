66056710c44731e852676f7ab6b2266a67fcd038
OneLemonyBoi <50998649+OneLemonyBoi@users.noreply.github.com>
2021-04-13 15:14:07 -0700

    Push

---
+++ src/main/java/onelemonyboi/lemonlib/LemonLib.java
@@ -0,0 +1,54 @@
+package onelemonyboi.lemonlib;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(LemonLib.MOD_ID)
+public class LemonLib {
+    public static final String MOD_ID = "lemonlib";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public LemonLib()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        this.preInit(event);
+        this.init(event);
+        this.postInit(event);
+    }
+    private void doClientStuff(final FMLClientSetupEvent event) // Render Stuff HERE!!
+    {
+    }
+
+    private void enqueueIMC(InterModEnqueueEvent event) {
+
+    }
+
+    private void preInit(FMLCommonSetupEvent event) {
+
+    }
+
+    private void init(FMLCommonSetupEvent event) {
+
+    }
+
+    private void postInit(FMLCommonSetupEvent event) {
+
+    }
+
+    public static Logger getLogger()
+    {
+        return LOGGER;
+    }
+}
